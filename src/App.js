7c6375daf2df2993b9e521bc7ec2814e37ce4994
jenny obryant <jennyobryant@protonmail.com>
2019-08-26 16:21:41 -0600

    fourth draft

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
+import Display from "./components/DisplayComponents/Display";
+
+
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
@@ -21,10 +24,11 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */
         }
-        <div className ="flex margin">
+        <div className ="flex">
           <div className = "flex column">
             <Specials />
             <Numbers />
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberButton = (props) => {
   return (
-    <div className ="button numberbutton">
+    <div className ={props.number == "0" ? "button numberbutton wide" : "button numberbutton"}>
       <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */
       props.number
